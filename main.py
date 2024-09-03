a765f07b47159400b2e16b37fe2512573bafbc70
takenusername <145628605+jeb1399@users.noreply.github.com>
2024-09-03 04:21:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import subprocess
+import os
 
 def run_node_server():
     try:
+        subprocess.run(['npm', 'install'], check=True)
         subprocess.run(['node', 'server.js'], check=True)
     except subprocess.CalledProcessError as e:
         print(f"An error occurred: {e}")
 
 if __name__ == '__main__':
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
     run_node_server()
